869420f3a0946ff5c95bc47026c02b2eee6c601c
unknown <vicse.ore@tecsup.edu.pe>
2019-09-19 23:57:26 -0500

    Primer commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,61 @@
+const { io } = require('../server');
+
+const { Usuarios } = require('../classes/usuarios');
+
+const { crearMensaje } = require('../utils/utilidades');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            });
+        }
+
+        //Para unir a una sala a usuarios
+        client.join(data.sala);
+
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
+
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+
+        return callback(usuarios.getPersonasPorSala(data.sala));
+
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    });
+
+    client.on('disconnect', () => {
+
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        //Para enviar a los todos los clientes que el usuario salio del chat
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salió `));
+
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
+
+
+    });
+
+    // Mensajes Privados
+    client.on('mensajePrivado', data => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+
+    });
+
+});
